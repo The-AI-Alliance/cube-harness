3265c8f70c5ae0936ed45f71dab8503dfca0f1f9
Oleh Shliazhko <oleh.shliazhko@servicenow.com>
2026-02-17 17:04:35 +0100

    fix recipe
    Signed-off-by: Oleh Shliazhko <oleh.shliazhko@servicenow.com>

--- recipes/miniwob_apriel.py
+++ recipes/miniwob_apriel.py
@@ -1,7 +1,9 @@
-import subprocess
+import os
 import sys
 import time
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 from agentlab2.agents.react import ReactAgentConfig
 from agentlab2.benchmarks.miniwob.benchmark import MiniWobBenchmark
@@ -10,20 +12,20 @@ from agentlab2.experiment import Experiment
 from agentlab2.llm import LLMConfig
 from agentlab2.tools.playwright import PlaywrightConfig
 
-# EAI_BASE_URL = "https://snow-research-rlar-vllm_apriel_1_6_inference5-8000.job.toolkit-sp.yul201.service-now.com/v1"
-EAI_BASE_URL = "https://snow-oleh_shliazhko-vllm_apriel_1_6_inference5-8000.job.console.elementai.com/v1"
+load_dotenv()  # Load environment variables from .env file
+
+VLLM_BASE_URL = os.environ["VLLM_BASE_URL"]
+VLLM_API_KEY = os.environ["VLLM_API_KEY"]
 
 
 def main(debug: bool):
     current_datetime = time.strftime("%Y%m%d_%H%M%S")
     output_dir = Path.home() / "agentlab_results" / "al2" / f"miniwob_{current_datetime}"
 
-    api_key = subprocess.check_output(["eai", "--profile", "yul101", "login", "token"], text=True).strip()
-
     llm_config = LLMConfig(
         model_name="openai/ServiceNow-AI/Apriel-1.6-15b-Thinker",
-        api_base=EAI_BASE_URL,
-        api_key=api_key,
+        api_base=VLLM_BASE_URL,
+        api_key=VLLM_API_KEY,
         temperature=1.0,
         num_retries=0,  # disable retries to see raw errors from Apriel
     )
