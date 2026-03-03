7ac4cb7ef81e9708eda7886e3427119287af25ba
nicolas.gontier <nicolas.gontier@servicenow.com>
2026-03-02 23:25:28 -0500

    add simple benchmark as a Cube
    Signed-off-by: nicolas.gontier <nicolas.gontier@servicenow.com>

---
+++ cubes/arithmetic-cube/src/arithmetic_cube/__init__.py
@@ -0,0 +1,15 @@
+from arithmetic_cube.tool import ArithmeticTool, ArithmeticToolConfig
+from arithmetic_cube.task import ArithmeticTaskConfig, SolveArithmeticTask
+from arithmetic_cube.benchmark import ArithmeticBenchmark
+from arithmetic_cube.debug import DebugAgent, get_debug_task_configs, make_debug_agent
+
+__all__ = [
+    "ArithmeticTool",
+    "ArithmeticToolConfig",
+    "ArithmeticTaskConfig",
+    "SolveArithmeticTask",
+    "ArithmeticBenchmark",
+    "DebugAgent",
+    "get_debug_task_configs",
+    "make_debug_agent",
+]
