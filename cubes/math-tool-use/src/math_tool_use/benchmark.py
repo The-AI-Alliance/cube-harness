10dd333315ef16bbac9689121792dd8e9e00dff1
Masih Aminbeidokhti <masih.aminbeidokhti@servicenow.com>
2026-04-27 13:48:54 +0000

    add more math dataset

--- cubes/math-tool-use/src/math_tool_use/benchmark.py
+++ cubes/math-tool-use/src/math_tool_use/benchmark.py
@@ -9,12 +9,33 @@ from math_tool_use.task import MathToolUseTaskConfig
 
 logger = logging.getLogger(__name__)
 
-_DATASET_NAME: str = (
-    "https://raw.githubusercontent.com/Open-Reasoner-Zero/Open-Reasoner-Zero/refs/heads/main/data/orz_math_57k_collected.json"
-)
+def process_aime_and_amc(dataset, dataset_name):
+    for item in dataset:
+        task = item["problem"]
+        answer = "\\boxed{" + str(item["answer"]) + "}"
+        yield {
+            "dataset": dataset_name,
+            "task": task,
+            "answer": answer,
+        }
+
+def _load_aime_dataset(year: int, upsample_factor: int = 0) -> list[dict]:
+    if year == 2025:
+        aime_dataset = load_dataset("MathArena/aime_2025", split="train", trust_remote_code=True)
+    else:
+        aime_dataset = load_dataset("AI-MO/aimo-validation-aime", split="train", trust_remote_code=True)
+        aime_dataset = aime_dataset.filter(lambda x: str(year) in x["url"])
+
+    dataset_name = f"aime_{year}" + ("" if upsample_factor > 0 else "_original")
+    tasks_data = [s for s in process_aime_and_amc(aime_dataset, dataset_name) if s is not None]
+
+    if upsample_factor > 0:
+        tasks_data *= upsample_factor
+
+    return tasks_data
 
 
-def _process_open_reasoner(dataset, dataset_name):
+def process_open_reasoner(dataset, dataset_name):
     for item in dataset:
         # Note: Open Reasoner tasks sometimes have preamble, e.g.
         # - Example 31 (2004 College Entrance Examination Hunan Paper)
@@ -27,39 +48,63 @@ def _process_open_reasoner(dataset, dataset_name):
 
 def load_task_metadata() -> dict[str, TaskMetadata]:
     """Load task metadata from the Open Reasoner Zero dataset."""
-    dataset = load_dataset(
-        "json",
-        data_files=_DATASET_NAME,
-        split="train",
-        trust_remote_code=True,
-    )
-    tasks_data = [s for s in _process_open_reasoner(dataset, "open_reasoner_zero_57k") if s is not None]
+
+    dataset_names = ["open_reasoner_zero_57k", "open_reasoner_zero_extended_72k", "aime_2025"]
+    dataset_urls = {
+        "open_reasoner_zero_57k": "https://raw.githubusercontent.com/Open-Reasoner-Zero/Open-Reasoner-Zero/refs/heads/main/data/orz_math_57k_collected.json",
+        "open_reasoner_zero_extended_72k": "https://raw.githubusercontent.com/Open-Reasoner-Zero/Open-Reasoner-Zero/refs/heads/main/data/orz_math_72k_collection_extended.json"
+    }
 
     metadata: dict[str, TaskMetadata] = {}
-    for i, t in enumerate(tasks_data):
-        instance_id = f"q_{i}"
-        metadata[instance_id] = TaskMetadata(
-            id=instance_id,
-            abstract_description="Solve math by tool-using with Python and submit final LaTeX answer via MathAnswer",
-            recommended_max_steps=3,
-            split="train",
-            extra_info={
-                "question": t["task"],
-                "expected": t["answer"],
-                "dataset": t["dataset"],
-                "rewards": {
-                    "correct_answer_finished": 1.0,
-                    "correct_answer_not_finished": 0,
-                    "wrong_answer_finished": 0,
-                    "wrong_answer_not_finished": 0,
-                    "no_answer_finished": 0,
-                    "no_answer_not_finished": 0,
-                    "unparsable_finished": 0,
-                    "unparsable_not_finished": 0}
-            },
-        )
 
-    logger.info(f"Loading Open Reasoner Zero dataset: {len(metadata)} tasks")
+    for dataset_name in dataset_names:
+        tasks_data = []
+        if "open_reasoner" in dataset_name:
+            dataset = load_dataset(
+                "json",
+                data_files=dataset_urls.get(dataset_name),
+                split="train",
+                trust_remote_code=True,
+            )
+            tasks_data = [s for s in process_open_reasoner(dataset, dataset_name) if s is not None]
+        
+        if "aime" in dataset_name:
+            year = int(dataset_name.split("_")[1])
+            if dataset_name.endswith("_original"):
+                tasks_data = _load_aime_dataset(year, upsample_factor=0)
+            else:               
+                tasks_data = _load_aime_dataset(year, upsample_factor=16)
+
+        if len(tasks_data) == 0:
+            logger.warning(f"No tasks loaded for dataset {dataset_name}")
+            continue
+
+        for i, t in enumerate(tasks_data):
+            instance_id = f"{dataset_name}_{i}"
+            metadata[instance_id] = TaskMetadata(
+                id=instance_id,
+                abstract_description="Solve math by tool-using with Python and submit final LaTeX answer via MathAnswer",
+                recommended_max_steps=3,
+                split="train",
+                extra_info={
+                    "dataset_name": dataset_name,
+                    "question": t["task"],
+                    "expected": t["answer"],
+                    "dataset": t["dataset"],
+                    "rewards": {
+                        "correct_answer_finished": 1.0,
+                        "correct_answer_not_finished": 0,
+                        "wrong_answer_finished": 0,
+                        "wrong_answer_not_finished": 0,
+                        "no_answer_finished": 0,
+                        "no_answer_not_finished": 0,
+                        "unparsable_finished": 0,
+                        "unparsable_not_finished": 0}
+                },
+            )
+
+            logger.info(f"Loading {dataset_name}: {len(tasks_data)} tasks")
+    logger.info(f"Loading total of {len(metadata)} tasks")
     return metadata
 
 class MathToolUseBenchmark(Benchmark):
